f221384d7c94d29c54d277ce59df40ec85f76cdf
JosephPerri <147216273+JosephPerri@users.noreply.github.com>
2023-10-06 21:00:49 -0400

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 from datetime import datetime
-import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
